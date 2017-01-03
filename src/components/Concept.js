0fa5e9aced09a54696e0588c9add9941ae6d924a
Erik Winge <erik.winge@ub.uio.no>
2017-01-03 11:11:29 +0100

    fix Concept attribute name

--- src/components/Concept.js
+++ src/components/Concept.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 
 export default function Concept({id,terms}) {
-    return(<li>{id + ": " + terms[0].lexicalValue} </li>);
+    return(<li>{id + ": " + terms[0].value} </li>);
 }
 
