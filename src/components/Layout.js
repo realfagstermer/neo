392110033fe92537b9b8afe5662c945d2a6451c3
Erik Winge <erik.winge@ub.uio.no>
2017-06-22 09:54:36 +0200

    cleanup layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,13 @@
 import React from "react";
 import NavigationBar from "./NavigationBar.js";
 
-//Outermost component. Renders the navigation bar
-//and everything else below it.
-var Layout = React.createClass({
-
-	render: function() {
-//		console.log(this.props.children)
-		return (
-			<div className="layout">
-				<NavigationBar className="nav" />
-				{this.props.children}
-			</div>
-		);
-	}
-});
-
-module.exports = Layout;
+// Outermost component. Renders the navigation bar
+// and everything else below it.
+export default function Layout(props) {
+	return (
+		<div className="layout">
+			<NavigationBar />
+			{props.children}
+		</div>
+	);
+}
