8e076f8274f7f3d8e1e45a02ec3618abedc40e53
Erik Winge <erik.winge@ub.uio.no>
2017-07-04 12:16:54 +0200

    edit users

--- src/actions/users.js
+++ src/actions/users.js
@@ -7,6 +7,7 @@ import { GET, FAILURE } from "./actions";
 export const GET_USER = "GET_USER";
 export const RECEIVE_USER= "RECEIVE_USER";
 export const RECEIVE_ALL_USERS = "RECEIVE_ALL_USERS";
+export const PUT_USER = "PUT_USER";
 
 // Action creators
 export function getAllUsers() {
@@ -29,7 +30,19 @@ export function getUser(id) {
     }
 }
 
+export function putUser(user) {
+    console.log("saving user", user);
+    return {
+      [CALL_API]: {
+        endpoint: `${serverURL}/users/${user.id}`,
+        method: 'PUT',
+        types: [PUT_USER, RECEIVE_USER, FAILURE]
+      }
+    }
+}
+
 
 // Bound action creators
 export const boundGetAllUsers = () => store.dispatch(getAllUsers());
 export const boundGetUser = id => store.dispatch(getUser(id));
+export const boundPutUser = user => store.dispatch(putUser(user));
--- src/components/users.js
+++ src/components/users.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
-import { boundGetUser } from "../actions/users";
+import { boundPutUser, boundGetUser } from "../actions/users";
 
 export function UserLink({id,username,name,created,createdBy}) {
     return <Link to= {`/users/${id}`}>{name}</Link>;
@@ -40,6 +40,7 @@ export class UserDetails extends Component {
                     <dt>Access:</dt>
                     <dd>{this.props.user.access}</dd>
                 </dl>
+                <Link to= {`/users/${this.props.user.id}/edit`}>Edit</Link>
             </div>
         );
 // }
@@ -50,10 +51,106 @@ export class UserDetails extends Component {
     }
 }
 
-export function UserEdit({id,username,name,created,createdBy}) {
-    // TODO
-    return(<li>{name}</li>);
+export class UserEdit extends Component {
+    constructor(props) {
+        super(props);
+        this.setField = this.setField.bind(this);
+        this.onSubmitClick = this.onSubmitClick.bind(this);
+        this.reset = this.reset.bind(this);
+        
+        // For clearing the form
+        this.emptyState = {
+            id: "",
+            name: '',
+            username: '',
+            created: new Date().toISOString(),
+            access: "read",
+        };
+        
+        this.state = Object.assign({}, this.emptyState);
+    }
+
+    onSubmitClick(event) {
+        event.preventDefault();
+        // update or new user?
+        if (this.state.id) {
+            boundPutUser(this.state)
+        } else {
+            boundUser(this.state);
+        }
+    }    
+
+    // clear previous state
+    reset() {
+// this.replaceState(Object.assign({}, this.emptyState));
+// this.setState({all:undefined,old:undefined,keys:undefined});
+        this.state = Object.assign({}, this.emptyState);
+        this.forceUpdate();
+    }
+
+    componentWillMount() {
+        boundGetUser(this.props.match.params.id);
+    }
+     
+    componentWillReceiveProps(nextProps) {
+        // get correct user
+        if (nextProps.location !== this.props.location) {
+            boundGetUser(nextProps.match.params.id);
+        }
+        this.setState(this.props.user);
+    }
+    
+
+    isFormValid() {
+        return (this.state.name && this.state.username);
+    }
+    
+    setField(field) {
+        return event => this.setState({ [field]: event.target.value });
+    }
+
+
+    render() {
+        console.log("Edit for:",this.props.user);
+        return(
+            <div className="UserEdit">
+            <form>
+                <div>
+                    <label>
+                        <span>Name</span>
+                        <input type="text" value={this.state.name} onChange={this.setField("name")} />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <span>Username</span>
+                        <input type="text" value={this.state.username} onChange={this.setField("username")} />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <span>Access</span>
+                        <select value={this.state.access} onChange={this.setField("access")}>
+                            <option value="read">read</option>
+                            <option value="classify">classify</option>
+                            <option value="admin">admin</option>
+                        </select>
+                    </label>
+                </div>
+                <div>
+                    <button disabled={!this.isFormValid()} id="submit" onClick={this.onSubmitClick}>Submit</button>
+                    <Link to= {`/users/${this.props.user.id}`}>Cancel</Link>
+                </div>
+            </form>
+            </div>
+        );
+    }
+    
+    static propTypes= {
+        user: PropTypes.object.isRequired,
+    }
 }
+
 
 export const UserList = ({ users }) => {
     return(
